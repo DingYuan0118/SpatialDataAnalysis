3ba0aa5603475d6c4ed3d19c112c7e2160924efe
Yuan <804547696@qq.com>
2020-08-13 12:24:53 +0800

    complete experiment3

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -2,7 +2,9 @@ from qgis.core import (
   QgsVectorLayer,
   QgsPointXY,
 )
-
+import re
+import argparse
+import time
 from qgis.core import *
 from qgis.gui import *
 from qgis.analysis import *
@@ -10,15 +12,42 @@ from qgis.analysis import *
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtGui import *
 
+
+def Node2QgisXY(start_node, end_node):
+    node_list = []
+    with open("sfo_nodes.txt", "r") as file:
+        for line in file.readlines():
+            node_list.append(line)
+
+    for i in range(len(node_list)):
+        node_list[i] = re.split(r'[:<>,\s]', node_list[i].rstrip())
+
+    for i in range(len(node_list)):
+        if int(node_list[i][0]) == start_node:
+            start_point = (float(node_list[i][-9]), float(node_list[i][-8]))
+
+        if int(node_list[i][0]) == end_node:
+            end_point = (float(node_list[i][-9]), float(node_list[i][-8]))
+    return start_point, end_point
+parse = argparse.ArgumentParser()
+parse.add_argument("--start_node", type=int, help="start node")
+parse.add_argument("--end_node", type=int, help="end node")
+parse.add_argument("--path_output", help="specify the shortest path for output file")
+parse.add_argument("--status_output", help="specify the status path for output file")
+arg = parse.parse_args()
+
+start = time.time()
 vectorLayer = QgsVectorLayer('E:/TaskAndAnswer/QGIS/experiment3/data-shape|layername=sfo_roads', 'lines')
 director = QgsVectorLayerDirector(vectorLayer, -1, '', '', '', QgsVectorLayerDirector.DirectionBoth)
 strategy = QgsNetworkDistanceStrategy()
 director.addStrategy(strategy)
 
 builder = QgsGraphBuilder(vectorLayer.sourceCrs())
+start_point, end_point = Node2QgisXY(arg.start_node, arg.end_node)
+print(start_point, end_point)
 
-startPoint = QgsPointXY(-13633266.525914, 4539887.836666)
-endPoint = QgsPointXY(-13632755.963284, 4539153.902885)
+startPoint = QgsPointXY(start_point[0], start_point[1])
+endPoint = QgsPointXY(end_point[0], end_point[1])
 
 tiedPoints = director.makeGraph(builder, [startPoint, endPoint])
 tStart, tStop = tiedPoints
@@ -43,11 +72,21 @@ while idxEnd != idxStart:
     idxEnd = graph.edge(tree[idxEnd]).fromVertex()
     route.insert(0, graph.vertex(idxEnd).point())
 
-# Display
-rb = QgsRubberBand(iface.mapCanvas())
-rb.setColor(Qt.red)
+# Display in the Qgis
+# rb = QgsRubberBand(iface.mapCanvas())
+# rb.setColor(Qt.red)
+#
+# # This may require coordinate transformation if project's CRS
+# # is different than layer's CRS
+# for p in route:
+#     rb.addPoint(p)
+end = time.time()
 
-# This may require coordinate transformation if project's CRS
-# is different than layer's CRS
-for p in route:
-    rb.addPoint(p)
+with open(arg.path_output, 'w') as file:
+    file.write("node {} to node {}\n".format(arg.start_node, arg.end_node))
+    for line in route:
+        file.write(str(line))
+        file.write("\n")
+
+with open(arg.status_output, "w") as file:
+    file.write('node {} to node {} spent {}s to compute'.format(arg.start_node, arg.end_node, (end - start)))
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# SpatialDataAnalysis
-Homework for Spatial data analysis
 
-## This repo is my homework for experiment3 of Spatial data analysis
+# This repo is my homework for experiment3 of Spatial data analysis
+
+## Enviroment
+ - Python3
+ - QGIS 3.14
+ - sfo_nodes.txt
+ - data-shape|layername=sfo_roads
+ 
+## Usage
+* run `python NetworkAnalysis.py --start_node  "输入节点" --end_node  "输出节点"  --path_output "最短路径" --status_output "状态文件"`  
+In the path_output file, you will get the shortest path computed by dijkstra algorithm.
